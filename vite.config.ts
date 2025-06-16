1a775f82008169df85cec95709fac774e8f5daf0
suman4132 <sk8760053@gmail.com>
2025-06-16 22:52:10 +0530

    Updated features

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/VigLabEquipmentSystem/',  // 👈 Your GitHub repo name here
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
